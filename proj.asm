3f3d10263173957fe8d3be20a77dc4c300e6e456
arrz.dev <55147716+arrzdev@users.noreply.github.com>
2022-06-02 00:03:44 +0100

    Added render of entitie goomba and made infinity map :sunrise_over_mountains:

--- proj.asm
+++ proj.asm
@@ -22,18 +22,16 @@ KEY_MAX_LIN EQU 8		; linha a testar (4ª linha)
 MASK EQU 0FH	
 
 ;keys
-KEY_0 EQU 00H
-KEY_4 EQU 04H
-
-;starting space ship coords
-X EQU 0
-Y EQU 25
+KEY_LEFT EQU 00H
+KEY_RIGHT EQU 02H
+KEY_DOWN EQU 0CH
 
 ;colors
 ;kinda nude 0FFAAH
 ;black 0F000H
+BLACK EQU 0F000H
 RED EQU 0FF00H
-BROWN EQU 0F000H
+BROWN EQU 0FA52H
 NUDE EQU 0FFB5H
 BLUE EQU 0F06FH
 WHITE EQU 0FFFFH
@@ -47,15 +45,48 @@ pilha:
 INITIAL_SP:
 
 ENTITIE_MARIO:
-  WORD 5 ;lenght value of space ship
-  WORD 5 ;height value of space ship
+  ;entitie position
+  WORD 30 ;default x
+  WORD 25 ;default y
+
+  ;entitie sprite index
+  WORD 0 ;default sprite index
+
+  ;entitie size
+  WORD 5 ;lenght value of caracter
+  WORD 5 ;height value of caracter
   
-  ;define colors of space ship
+  ;sprite 0
   WORD 0, RED, RED, RED, RED		
-  WORD 0, BROWN, NUDE, NUDE, 0
+  WORD 0, BLACK, NUDE, NUDE, 0
   WORD RED, BLUE, RED, BLUE, RED
   WORD WHITE, BLUE, BLUE, BLUE, WHITE
-  WORD 0, BROWN, 0, BROWN, 0
+  WORD 0, BLACK, 0, BLACK, 0
+
+  ;sprite 1
+  WORD 0, BLUE, BLUE, BLUE, BLUE		
+  WORD 0, BLACK, NUDE, NUDE, 0
+  WORD BLUE, BLUE, BLUE, BLUE, BLUE
+  WORD WHITE, BLUE, BLUE, BLUE, WHITE
+  WORD 0, BLACK, 0, BLACK, 0
+
+ENTITIE_GOOMBA:
+  WORD 30 ;default x
+  WORD 0 ;default y
+
+  ;entitie sprite index
+  WORD 0 ;default sprite index
+
+  ;entitie size
+  WORD 5 ;lenght value of caracter
+  WORD 5 ;height value of caracter
+
+  ;sprite 0
+  WORD 0, 0, RED, 0, 0
+  WORD 0, RED, WHITE, RED, 0
+  WORD RED, WHITE, RED, RED, RED
+  WORD 0, BLACK, NUDE, BLACK, 0
+  WORD 0, NUDE, NUDE, NUDE, 0
 
 
 ;codigo
@@ -72,15 +103,15 @@ setup:
   ;initial set for momentum
   
 start:
-  ;set starting render position 
-  MOV R1, X 
-  MOV R2, Y
-
-  ;set starting sprite and write action
-  MOV R3, ENTITIE_MARIO ;set sprite table to be rendered
+  ;render initial entities
   MOV R8, 1 ;set action to "write"
 
-  ;first render of the space ship
+  ;render mario
+  MOV R3, ENTITIE_MARIO ;set sprite table to be rendered
+  CALL render_sprite
+
+  ;render goomba
+  MOV R3, ENTITIE_GOOMBA
   CALL render_sprite
 
   ;start keyboard listen
@@ -94,20 +125,32 @@ render_sprite:
   ; R4 is the sprite length
   ; R5 is the sprite height
 
+  PUSH R0
   PUSH R1
   PUSH R2
-  PUSH R3 ;save table address
+  PUSH R3
   PUSH R4
   PUSH R5
+  PUSH R6
+  PUSH R7
 
-  MOV R4, [R3] ;get lenght of sprite
-  MOV R5, [R3+2] ;get height of sprite
+  ;get entitie position
+  MOV R1, [R3] ;x
+  MOV R2, [R3+2] ;y
+
+  ;get selected sprite
+  MOV R7, [R3+4]
+
+  ;get entitie length
+  MOV R4, [R3+6] ;get entitie length
+  MOV R5, [R3+8] ;get entitie height
 
   ;get first pixel
-  ADD R3, 2 ;move to starting point of pixel rendering
-  ; R3 is set to be the pixel before 
+  MOV R0, 8
+  ADD R3, R0 ;move to starting point of sprite rendering
+  ;R3 is set to be the address before 
   
-  MOV R6, R4 ;value of current index color of pixel to render
+  MOV R6, R4 ;starting iterator value
   
   render_line:
     ; loop to render line of pixels
@@ -124,11 +167,14 @@ render_sprite:
     SUB R5, 1 ;decrement height of sprite
     JNZ render_line
 
-  POP  R5
-  POP  R4
-  POP  R3
-  POP  R2
-  POP  R1
+  POP R7
+  POP R6
+  POP R5
+  POP R4
+  POP R3
+  POP R2
+  POP R1
+  POP R0
   RET
 
 render_pixel:
@@ -156,37 +202,77 @@ render_pixel:
 
 handle_keyboard:
   PUSH R0 ;R0 - column to test
+  PUSH R1 ;max background index
+  PUSH R2 ;temp store background value
   PUSH R6 ;R6 - line to test (1, 2, 4 ou 8)
+
+  MOV R1, 8 ;set max background index
 
   MOV R6, 1 ;first line to test 
 
   test_line:
     CALL listen_keyboard_line
     CMP R0, -1 ;if not pressed
-    JNZ key_found
+    JNZ handle_actions
     ROL R6, 1
     JMP test_line
   
-  key_found:
-    CMP R0, KEY_0
-    JZ key_0_func
+  handle_actions:
+    CMP R0, KEY_LEFT
+    JZ move_mario_left
 
-    CMP R0, KEY_4
-    JZ key_4_func
+    CMP R0, KEY_RIGHT
+    JZ move_mario_right
 
     ;the key doesn't have any action associated
     JMP return_handle
 
-  ; Move space ship left
-  key_0_func:
+  move_mario_left:
+    ;set action entitie as mario
+    MOV R3, ENTITIE_MARIO
+
+    ;get current background index
+    MOV R0, [SET_BACKGROUND]
+    CMP R0, 0
+    JZ cycle_background_left
+
+    ADD R0, -1; increment background index
+    JMP set_background_left; 
+
+    cycle_background_left:
+      MOV R0, R1 ;set background to max index
+
+    set_background_left:
+      MOV [SET_BACKGROUND], R0 ;set new background
+
+    ;move
     MOV R7, -1 ;set momentum to -1
+
     CALL check_left_boundary
     CALL movement
     JMP return_handle
 
-  ; Move space ship right
-  key_4_func:
+  move_mario_right:
+    ;set action entitie as mario
+    MOV R3, ENTITIE_MARIO
+
+    ;get current background index
+    MOV R0, [SET_BACKGROUND]
+    CMP R1, R0
+    JZ cycle_background_right
+
+    ADD R0, 1; increment background index
+    JMP set_background_right; 
+
+    cycle_background_right:
+      MOV R0, 0 ;set background back to 0
+
+    set_background_right:
+      MOV [SET_BACKGROUND], R0 ;set new background
+
+    ;move
     MOV R7, +1 ;set momentum to +1
+
     CALL check_right_boundary
     CALL movement
     JMP return_handle ;the first elif dont need a jump..
@@ -194,6 +280,8 @@ handle_keyboard:
 
   return_handle:
     POP R6
+    POP R2
+    POP R1
     POP R0
     RET
 
@@ -227,19 +315,27 @@ listen_keyboard_line:
 ; R7 = +1 -> move right
 ; R7 = -1 -> move left
 movement:
+  PUSH R0
+
   CMP R7, 0
   JZ return_movement ;if R7 is set to 0, then don't move
 
   ;otherwise start by deleting the old sprite
   MOV R8, 0 ;set action to "delete"
   CALL render_sprite ; delete sprite with action setted previously
+  
+  ;get entitie x position
+  MOV R0, [R3]
+  ADD R0, R7 ;move entitie one pixel to setted direction
 
-  ADD R1, R7 ;move sprite one pixel to setted direction
+  ;update position on entitie scope
+  MOV [R3], R0
 
   MOV R8, 1 ;set action to "write"
   CALL render_sprite ;render sprite with action setted previously
 
   return_movement:
+    POP R0
     RET
 
 check_right_boundary:
@@ -247,8 +343,11 @@ check_right_boundary:
   PUSH R5
   PUSH R6
 
-  MOV R5, MAX_SCREEN_WIDTH ;get sprite table
-  MOV R6, [R3] ;get lenght of sprite
+  ;get entitie X position
+  MOV R1, [R3]
+
+  MOV R5, MAX_SCREEN_WIDTH ;get screen max width
+  MOV R6, [R3+6] ;get lenght of the entitie
 
   ADD R1, R6 ;get sprite right edge
 
@@ -267,6 +366,9 @@ check_right_boundary:
 
 check_left_boundary:
   PUSH R5
+
+  ;get entitie X postion
+  MOV R1, [R3]
 
   MOV R5, MIN_SCREEN_WIDTH ;get screen min width
   
@@ -318,11 +420,11 @@ convert_to_key:
 normalize_index:
   MOV R9, -1 ;init counter as -1
 
-    not_zero:
-      ADD R9, 1 ;increment counter
-      SHR R0, 1 ;shift to the right
-      CMP R0, 0
-      JGT not_zero ;if not zero then continue
+  not_zero:
+    ADD R9, 1 ;increment counter
+    SHR R0, 1 ;shift to the right
+    CMP R0, 0
+    JGT not_zero ;if not zero then continue
     
   RET
 
